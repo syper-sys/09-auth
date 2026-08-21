95ffe068943d94cdc200d5f3089165e7f41e93b6
seroga <sergijvchichur@gmail.com>
2026-08-21 03:34:31 +0300

    init

--- app/(private routes)/notes/action/create/page.tsx
+++ app/(private routes)/notes/action/create/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import css from '@/app/(private routes)/notes/action/create/page.module.css';
+import css from './page.module.css';
 import NoteForm from '@/components/NoteForm/NoteForm';
 
 export const metadata: Metadata = {
